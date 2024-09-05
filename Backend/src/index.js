d0e7ba9c8ce7ddebb3128f8c4c7e5ed17c09619b
Syed Hisham Shah <syedhishamshah27@gmail.com>
2024-09-05 17:28:31 +0500

    add debugging

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,4 +1,3 @@
-// index.js (src)
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 import { app } from "./app.js"; // Import the app object
@@ -9,7 +8,7 @@ dotenv.config({
 
 connectDB()
   .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
+    app.listen(process.env.PORT || 8080, () => {
       console.log(`Server is running at PORT:${process.env.PORT}`);
     });
   })
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,14 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { ThemeProvider } from "@material-tailwind/react";
-import { AuthProvider } from "./context/AuthProvider.jsx"; // Import the AuthProvider
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+      <App />
     </ThemeProvider>
   </React.StrictMode>
 );
