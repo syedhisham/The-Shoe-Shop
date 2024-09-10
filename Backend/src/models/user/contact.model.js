4d898fab3e4d14e769ba8103b3395cdcf0931850
Syed Hisham Shah <syedhishamshah27@gmail.com>
2024-09-10 16:24:43 +0500

    upd contact model

--- Backend/src/models/user/contact.model.js
+++ Backend/src/models/user/contact.model.js
@@ -4,7 +4,6 @@ const contactSchema = new mongoose.Schema({
   phone: {
     type: String,
     required: true,
-    unique: true,
     match: [/^(\+92|0)?3\d{9}$/, "is invalid"],
   },
   phoneVerificationCode: {
