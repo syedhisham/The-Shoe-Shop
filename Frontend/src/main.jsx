10dcf296e147b1c3496cffe0814dbf9d8dca2e1e
Syed Hisham Shah <syedhishamshah27@gmail.com>
2024-09-17 13:44:13 +0500

    add Product provider and Navigation Provider

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,11 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { ThemeProvider } from "@material-tailwind/react";
+import { ProductProvider } from "./context/ProductContext.jsx";
+import { NavigationProvider } from "./context/NavigationContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider>
-      <App />
+      <NavigationProvider>
+        <ProductProvider>
+          <App />
+        </ProductProvider>
+      </NavigationProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
