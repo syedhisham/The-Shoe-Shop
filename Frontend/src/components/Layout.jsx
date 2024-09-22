7188ee73dbefcc62524187c0c7b965ff932c5e24
Syed Hisham Shah <syedhishamshah27@gmail.com>
2024-09-23 00:00:53 +0500

    add a Layout

--- Frontend/src/components/Layout.jsx
+++ Frontend/src/components/Layout.jsx
@@ -1,20 +1,18 @@
-import React from 'react'
-import Header from './Header'
-import Footer from './Footer'
-import { Outlet } from 'react-router-dom'
+import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
+import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
   return (
-    <div className="layout">
-      <div className="main-content">
-        <Header/>
-        <div className="content">
-          <Outlet />
-        </div>
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <div className="flex-grow">
+        <Outlet />
       </div>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
