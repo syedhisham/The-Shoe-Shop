2052c39e7555d74da9ed46350bbdf4fdb4451464
Syed Hisham Shah <syedhishamshah27@gmail.com>
2024-09-24 23:26:43 +0500

    upd cart to include inventory status

--- Frontend/src/pages/Cart.jsx
+++ Frontend/src/pages/Cart.jsx
@@ -4,12 +4,18 @@ import axios from "axios";
 import LoadingOverlay from "../components/LoadingOverlay";
 import ErrorToast from "../components/ErrorToast";
 import SuccessToast from "../components/SuccessToast";
+import { Button } from "@material-tailwind/react";
 
 const Cart = () => {
   const [cartItems, setCartItems] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const navigate = useNavigate();
+  const [inventoryStatus, setInventoryStatus] = useState({
+    textColor: "",
+    circleColor: "",
+    text: "",
+  });
 
   const userId = localStorage.getItem("userId");
 
@@ -47,6 +53,35 @@ const Cart = () => {
     fetchCartItems();
   }, [userId]);
 
+  useEffect(() => {
+    if (cartItems) {
+      const product = cartItems.map((item) => item.productDetails);
+      console.log("This is product logged", product.stock);
+
+      let status = { textColor: "", circleColor: "", text: "" };
+      if (product.stock < 15) {
+        status = {
+          textColor: "text-red-500",
+          circleColor: "bg-red-500",
+          text: "Limited stock available",
+        };
+      } else if (product.stock >= 15 && product.stock < 30) {
+        status = {
+          textColor: "text-yellow-500",
+          circleColor: "bg-yellow-500",
+          text: "Stock running low",
+        };
+      } else {
+        status = {
+          textColor: "text-green-500",
+          circleColor: "bg-green-500",
+          text: "In stock",
+        };
+      }
+      setInventoryStatus(status);
+    }
+  }, [cartItems]);
+
   const handleDeleteItem = async (productId) => {
     try {
       await axios.delete(
@@ -76,20 +111,22 @@ const Cart = () => {
     }
   };
 
-  const handleUpdateItem = (item) => {
-    navigate(`/detailedProduct/${item}`);
+  const handleUpdateItem = (item, size, color) => {
+    navigate(`/detailedProduct/${item}`, {
+      state: {
+        selectedSize: size,
+        selectedColor: color,
+      },
+    });
   };
 
   const handleCheckout = () => {
-    navigate(`/checkout`); 
+    navigate(`/checkout`);
   };
 
   if (loading) {
     return <LoadingOverlay />;
   }
-
-const itemsArray = cartItems.map((item) =>   console.log("This is cart items",item.product))
-  
 
   return (
     <div className="max-w-screen-2xl mx-auto p-5">
@@ -100,41 +137,66 @@ const itemsArray = cartItems.map((item) =>   console.log("This is cart items",it
         <div className="flex flex-col space-y-4">
           {cartItems.map((item, index) => (
             <div
-            key={`${item.productDetails._id}-${index}`}
+              key={`${item.productDetails._id}-${index}`}
               className="flex flex-col sm:flex-row justify-center items-start border border-gray-300 p-4 rounded-lg shadow-md bg-white"
             >
               {item.productImages.length > 0 && (
                 <img
                   src={item.productImages[0].imageUrl}
                   alt={item.productDetails.name}
-                  className="w-24 h-24 object-cover rounded mr-4 mb-4 sm:mb-0"
+                  className="w-36 h-36 object-cover rounded mr-4 mb-4 sm:mb-0"
                 />
               )}
-              <div className="flex flex-col flex-grow justify-center">
-                <h2 className="text-xl font-semibold mb-1">
-                  {item.productDetails.name}
-                </h2>
-                <p className="text-gray-700">Color: {item.color}</p>
-                <p className="text-gray-700">Size: {item.size}</p>
-                <p className="text-gray-700">Quantity: {item.quantity}</p>
+              <div className="flex flex-col flex-grow justify-center mt-2">
+                <div className="flex gap-5 items-center">
+                  <h2 className="text-xl font-semibold mb-1">
+                    {item.productDetails.name.toUpperCase()}
+                  </h2>
+
+                  <div className="flex items-center -mt-1">
+                    <div
+                      className={`w-2 h-2 rounded-full ${inventoryStatus.circleColor}`}
+                    ></div>
+                    <p className={`ml-2 ${inventoryStatus.textColor}`}>
+                      {inventoryStatus.text}
+                    </p>
+                  </div>
+                </div>
+
+                <p className="font-semibold text-gray-700 text-sm">
+                  Color:
+                  <span className="font-normal"> {item.color}</span>
+                </p>
+                <p className="font-semibold text-gray-700 text-sm">
+                  Size:
+                  <span className="font-normal"> {item.size}</span>
+                </p>
+                <p className="font-semibold text-gray-700 text-sm">
+                  Quantity:
+                  <span className="font-normal"> {item.quantity}</span>
+                </p>
                 <p className="text-gray-700 font-bold mt-2">
                   Price: Rs {item.productDetails.price * item.quantity}
                 </p>
               </div>
 
-              <div className="flex flex-col space-y-2 sm:ml-4">
-                <button
-                  onClick={() => handleUpdateItem(item.product)}
-                  className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition"
+              <div className="flex flex-col my-auto space-y-2 sm:ml-4">
+                <Button
+                  variant="text"
+                  onClick={() =>
+                    handleUpdateItem(item.product, item.size, item.color)
+                  }
+                  className="border"
                 >
                   Update
-                </button>
-                <button
+                </Button>
+                <Button
+                  variant="text"
                   onClick={() => handleDeleteItem(item.product)}
-                  className="bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600 transition"
+                  className="border"
                 >
                   Remove
-                </button>
+                </Button>
               </div>
             </div>
           ))}
@@ -143,15 +205,14 @@ const itemsArray = cartItems.map((item) =>   console.log("This is cart items",it
         <div className="text-center text-gray-500">Your cart is empty.</div>
       )}
 
-      {/* Checkout Button */}
       {cartItems.length > 0 && (
         <div className="text-right mt-6">
-          <button
+          <Button
             onClick={handleCheckout}
-            className="bg-green-500 text-white px-6 py-3 rounded hover:bg-green-600 transition"
+            className="bg-gray-800 text-white px-6 py-3 rounded hover:bg-gray-900 transition-all duration-300 ease-in-out"
           >
             Checkout
-          </button>
+          </Button>
         </div>
       )}
     </div>
